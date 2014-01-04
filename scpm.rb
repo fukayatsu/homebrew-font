512c791bcf559f9235c727017ef21b99f22958ae
fukayatsu <fukayatsu@gmail.com>
2014-01-05 00:08:44 +0900

    prototype
        diff --git a/scpm.rb b/scpm.rb
        index a716ee8..17eb51b 100644
        --- a/scpm.rb
        +++ b/scpm.rb
        @@ -23,6 +23,7 @@ class Scpm < Formula
           depends_on 'fontforge'
    
           def install
        +    require 'pry'; binding.pry
             share_fonts = share + 'fonts'
    
             Migu1MFonts.new.brew { share_fonts.install Dir['*.ttf'] }
    
    commit 2b5a71e359793f6a5a343d337e3848b26129d0ed
    Author: fukayatsu <fukayatsu@gmail.com>
    Date:   Sat Jan 4 23:36:02 2014 +0900
    
        fix path
    
    commit 8f81e6029a6fe951faa9ccebc76aa344e35ab2e8
    Author: fukayatsu <fukayatsu@gmail.com>
    Date:   Sat Jan 4 23:10:16 2014 +0900
    
        Change class name
    
    commit fc78cd1537d84507c271a951a4d9653dd2190681
    Author: fukayatsu <fukayatsu@gmail.com>
    Date:   Sat Jan 4 23:08:54 2014 +0900
    
        fix typo
    
    commit 528a74fcf02a994ecffb54d52a5efb6b4056bbd9
    Author: fukayatsu <fukayatsu@gmail.com>
    Date:   Sat Jan 4 23:05:48 2014 +0900
    
        [from now] 2014/01/04 23:05:48
    
    commit a0662e765e5136b917d9634cc35b85f8bb1ff987
    Author: fukayatsu <fukayatsu@gmail.com>
    Date:   Sat Jan 4 22:48:56 2014 +0900
    
        [from now] 2014/01/04 22:48:56

---
+++ scpm.rb
@@ -0,0 +1,49 @@
+require 'formula'
+
+class Migu1MFonts < Formula
+  homepage 'http://mix-mplus-ipa.sourceforge.jp/'
+  url      'http://sourceforge.jp/frs/redir.php?m=iij&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-1m-20130617.zip'
+  sha1     'a0641894cec593f8bb1e5c2bf630f20ee9746b18'
+  version  '20130617'
+end
+
+class SourceCodeProFonts < Formula
+  homepage 'http://sourceforge.net/projects/sourcecodepro.adobe/'
+  url      'http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip'
+  sha1     'c0e3f6f8e25b434c0e28a817539632f8a5ecb9e5'
+  version  '1.017'
+end
+
+class Scpm < Formula
+  homepage 'https://github.com/fukayatsu/scpm'
+  url      'https://github.com/fukayatsu/scpm/archive/v0.0.1.tar.gz'
+  sha1     '05a830d68b7c9c317099f69f73adbd4926c1c77b'
+  version  'v0.0.1'
+
+  depends_on 'fontforge'
+
+  def install
+    Migu1MFonts.new.brew        { buildpath.install Dir['*.ttf'] }
+    SourceCodeProFonts.new.brew { buildpath.install Dir['TTF/*.ttf'] }
+
+    system 'fontforge', './scpm.pe'
+
+    share_fonts = share + 'fonts'
+    share_fonts.install Dir['SCPM*.ttf']
+  end
+
+  test do
+    system 'false'
+  end
+
+  def caveats; <<-EOS.undent
+    ***************************************************
+    Generated files:
+      #{Dir[share+'fonts/SCPM*.ttf'].join("\n      ")}
+    ***************************************************
+    To install SCPM:
+      $ cp -f #{share}/fonts/SCPM*.ttf ~/Library/Fonts/
+    ***************************************************
+    EOS
+  end
+end
