a0662e765e5136b917d9634cc35b85f8bb1ff987
fukayatsu <fukayatsu@gmail.com>
2014-01-04 22:48:56 +0900

    [from now] 2014/01/04 22:48:56

---
+++ scpm.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Migu1MFonts < Formula
+  homepage 'http://mix-mplus-ipa.sourceforge.jp/'
+  url      'http://sourceforge.jp/frs/redir.php?m=iij&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-1m-20130617.zip'
+  sha1     'a0641894cec593f8bb1e5c2bf630f20ee9746b18'
+  version  '20130617'
+end
+
+class SourceCodeProFonts < Formula
+  homepage 'http://sourceforge.net/projects/sourcecodepro.adobe/'
+  url      'http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip'
+  sha1     'c0e3f6f8e25b434c0e28a817539632f8a5ecb9e5'
+  version  '1.017'
+end
